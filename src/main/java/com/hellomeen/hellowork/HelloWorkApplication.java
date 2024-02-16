1815f6d41545cafe1204568bad2cba67a245057a
minhyeongKang <kang7937@gmail.com>
2024-02-16 13:21:37 +0900

    first commit

---
+++ src/main/java/com/hellomeen/hellowork/HelloWorkApplication.java
@@ -0,0 +1,13 @@
+package com.hellomeen.hellowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hellomeen/hellowork/HelloWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellomeen.hellowork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
